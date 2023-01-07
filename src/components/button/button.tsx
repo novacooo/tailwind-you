ef06673c4ff98ec0af8950e8874200c2e88768a4
Jacek Nowak <jaceknowak226@gmail.com>
2023-01-08 00:49:54 +0100

    feat: add filled button enabled and disabled state styles

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,13 +1,15 @@
-interface ButtonProps {
-  children: string;
-}
+import { ButtonHTMLAttributes } from 'react';
 
-export const Button = ({ children }: ButtonProps) => (
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export const Button = ({ children, ...rest }: ButtonProps) => (
   <button
     type="button"
-    className="bg-primary hover:elevation-light-1 text-on-primary group relative overflow-hidden rounded-full px-6 py-3 font-medium tracking-wider disabled:bg-red-500"
+    className="bg-primary hover:elevation-light-1 text-label-lg text-on-primary disabled:text-on-surface group relative overflow-hidden rounded-full px-6 py-2.5 disabled:bg-transparent disabled:opacity-[.38] disabled:shadow-none"
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...rest}
   >
-    <div className="bg-on-primary absolute inset-0 opacity-0 group-hover:opacity-[.08] group-focus:opacity-[.12] group-active:opacity-[.08]" />
+    <div className="bg-on-primary group-disabled:bg-on-surface absolute inset-0 opacity-0 group-hover:opacity-[.08] group-focus:opacity-[.12] group-active:opacity-[.08] group-disabled:opacity-[.12]" />
     {children}
   </button>
 );
