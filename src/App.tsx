2b74b03ecec397bd6f9c2d40ed668876b8c71236
Jacek Nowak <jaceknowak226@gmail.com>
2023-01-08 00:55:14 +0100

    feat: filled Button done

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ const theme = themeFromSourceColor(argbFromHex('#d8919d'));
 applyTheme(theme, { target: document.body, dark: false });
 
 export const App = () => (
-  <div className="bg-background flex h-screen w-full flex-col items-center justify-center gap-6">
-    {/* <h1 className="text-display-lg">Display large</h1>
+  <div className="bg-background flex h-screen w-full flex-col items-center justify-center gap-4">
+    <h1 className="text-display-lg">Display large</h1>
     <h1 className="text-display-md">Display medium</h1>
     <h1 className="text-display-sm">Display small</h1>
     <h1 className="text-headline-lg">Headline large</h1>
@@ -26,7 +26,7 @@ export const App = () => (
     <h1 className="text-label-sm">Label small</h1>
     <h1 className="text-body-lg">Body large</h1>
     <h1 className="text-body-md">Body medium</h1>
-    <h1 className="text-body-sm">Body small</h1> */}
+    <h1 className="text-body-sm">Body small</h1>
     <Button>Enabled</Button>
     <Button disabled>Disabled</Button>
   </div>
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
@@ -6,7 +7,6 @@ export const Button = ({ children, ...rest }: ButtonProps) => (
   <button
     type="button"
     className="bg-primary hover:elevation-light-1 text-label-lg text-on-primary disabled:text-on-surface group relative overflow-hidden rounded-full px-6 py-2.5 disabled:bg-transparent disabled:opacity-[.38] disabled:shadow-none"
-    // eslint-disable-next-line react/jsx-props-no-spreading
     {...rest}
   >
     <div className="bg-on-primary group-disabled:bg-on-surface absolute inset-0 opacity-0 group-hover:opacity-[.08] group-focus:opacity-[.12] group-active:opacity-[.08] group-disabled:opacity-[.12]" />
