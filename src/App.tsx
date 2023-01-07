9dcbc518ab92e992c69be90e3fc1c3b47bf559cf
Jacek Nowak <jaceknowak226@gmail.com>
2023-01-07 21:49:28 +0100

    feat: add @material/material-color-utilities package and apply theme method

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import {
+  applyTheme,
+  argbFromHex,
+  themeFromSourceColor,
+} from '@material/material-color-utilities';
+
+// material theme apply
+const theme = themeFromSourceColor(argbFromHex('#9c1aff'));
+const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+applyTheme(theme, { target: document.body, dark: systemDark });
+
 export const App = () => (
   <div>
     <h1 className="text-3xl font-bold underline">App</h1>
