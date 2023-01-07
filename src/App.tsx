b84a459499afb6dffd07d64aec7564a66b3b767b
Jacek Nowak <jaceknowak226@gmail.com>
2023-01-08 00:27:53 +0100

    feat: add typography classes

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,22 @@ const theme = themeFromSourceColor(argbFromHex('#7b8f42'));
 applyTheme(theme, { target: document.body, dark: false });
 
 export const App = () => (
-  <div className="bg-background flex h-screen w-full items-center justify-center">
-    <Button>Example button</Button>
+  <div className="bg-background flex h-screen w-full flex-col items-center justify-center gap-6">
+    <h1 className="text-display-lg">Display large</h1>
+    <h1 className="text-display-md">Display medium</h1>
+    <h1 className="text-display-sm">Display small</h1>
+    <h1 className="text-headline-lg">Headline large</h1>
+    <h1 className="text-headline-md">Headline medium</h1>
+    <h1 className="text-headline-sm">Headline small</h1>
+    <h1 className="text-title-lg">Title large</h1>
+    <h1 className="text-title-md">Title medium</h1>
+    <h1 className="text-title-sm">Title small</h1>
+    <h1 className="text-label-lg">Label large</h1>
+    <h1 className="text-label-md">Label medium</h1>
+    <h1 className="text-label-sm">Label small</h1>
+    <h1 className="text-body-lg">Body large</h1>
+    <h1 className="text-body-md">Body medium</h1>
+    <h1 className="text-body-sm">Body small</h1>
+    <Button>Elevated</Button>
   </div>
 );
