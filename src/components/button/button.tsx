e00ebf48cd5286d225896af6c27b6aa0e7734879
Jacek Nowak <jaceknowak226@gmail.com>
2023-01-07 23:26:58 +0100

    feat: add dark elevation classes

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 export const Button = ({ children }: ButtonProps) => (
   <button
     type="button"
-    className="bg-primary hover:elevation-1 text-on-primary rounded-full px-6 py-3 font-medium tracking-wider"
+    className="bg-primary hover:elevation-light-1 text-on-primary dark:hover:elevation-dark-1 rounded-full px-6 py-3 font-medium tracking-wider"
   >
     {children}
   </button>
