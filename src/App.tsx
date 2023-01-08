6f6883cc21ce324967791aa96117b2b762a941fd
Jacek Nowak <jaceknowak226@gmail.com>
2023-01-08 01:41:47 +0100

    feat: add tonal button variant

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,13 @@ import {
 
 import { Button } from './components/button';
 
-const theme = themeFromSourceColor(argbFromHex('#d8919d'));
+const theme = themeFromSourceColor(argbFromHex('#c5e05a'));
 // const systemDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 applyTheme(theme, { target: document.body, dark: false });
 
 export const App = () => (
   <div className="bg-background flex h-screen w-full flex-col items-center justify-center gap-4">
-    <h1 className="text-display-lg">Display large</h1>
+    {/* <h1 className="text-display-lg">Display large</h1>
     <h1 className="text-display-md">Display medium</h1>
     <h1 className="text-display-sm">Display small</h1>
     <h1 className="text-headline-lg">Headline large</h1>
@@ -26,8 +26,12 @@ export const App = () => (
     <h1 className="text-label-sm">Label small</h1>
     <h1 className="text-body-lg">Body large</h1>
     <h1 className="text-body-md">Body medium</h1>
-    <h1 className="text-body-sm">Body small</h1>
-    <Button>Enabled</Button>
-    <Button disabled>Disabled</Button>
+    <h1 className="text-body-sm">Body small</h1> */}
+    <Button>Filled enabled</Button>
+    <Button disabled>Filled disabled</Button>
+    <Button variant="tonal">Tonal enabled</Button>
+    <Button variant="tonal" disabled>
+      Tonal disabled
+    </Button>
   </div>
 );
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,15 +1,35 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable react/jsx-props-no-spreading */
+import { clsx } from 'clsx';
 import { ButtonHTMLAttributes } from 'react';
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'filled' | 'outlined' | 'text' | 'elevated' | 'tonal';
+}
 
-export const Button = ({ children, ...rest }: ButtonProps) => (
+export const Button = ({
+  children,
+  variant = 'filled',
+  type,
+  ...rest
+}: ButtonProps) => (
   <button
-    type="button"
-    className="bg-primary hover:elevation-light-1 text-label-lg text-on-primary disabled:text-on-surface group relative overflow-hidden rounded-full px-6 py-2.5 disabled:bg-transparent disabled:opacity-[.38] disabled:shadow-none"
+    type={type || 'button'}
+    className={clsx(
+      'hover:elevation-light-1 text-label-lg disabled:text-on-surface group relative overflow-hidden rounded-full px-6 py-2.5 disabled:bg-transparent disabled:opacity-[.38] disabled:shadow-none',
+      variant === 'filled' && 'bg-primary text-on-primary',
+      variant === 'tonal' &&
+        'bg-secondary-container text-on-secondary-container',
+    )}
     {...rest}
   >
-    <div className="bg-on-primary group-disabled:bg-on-surface absolute inset-0 opacity-0 group-hover:opacity-[.08] group-focus:opacity-[.12] group-active:opacity-[.08] group-disabled:opacity-[.12]" />
+    <div
+      className={clsx(
+        'group-disabled:bg-on-surface absolute inset-0 opacity-0 group-hover:opacity-[.08] group-focus:opacity-[.12] group-active:opacity-[.08] group-disabled:opacity-[.12]',
+        variant === 'filled' && 'bg-on-primary',
+        variant === 'tonal' && 'bg-on-secondary-container',
+      )}
+    />
     {children}
   </button>
 );
